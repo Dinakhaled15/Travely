0409566e81d8285564ba2563a2f559167fc776d0
“dinakhaled15” <dinakhaled1999@outlook.com>
2023-01-08 10:17:58 +0300

    HIGH

---
+++ Nano/TEST.swift
@@ -0,0 +1,20 @@
+//
+//  TEST.swift
+//  Nano
+//
+//  Created by Dina Alhajj Ibrahim on 08/01/2023.
+//
+
+import SwiftUI
+
+struct TEST: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TEST_Previews: PreviewProvider {
+    static var previews: some View {
+        TEST()
+    }
+}
