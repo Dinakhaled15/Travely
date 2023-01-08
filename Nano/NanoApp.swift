7d8654c79e868212ab9ceff790b7491f0d5fe00f
Dina <Dinakhaled1999@outlook.com>
2023-01-08 09:06:26 +0300

    Initial Commit

---
+++ Nano/NanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoApp.swift
+//  Nano
+//
+//  Created by Dina Alhajj Ibrahim on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
