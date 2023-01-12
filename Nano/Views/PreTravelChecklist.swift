8b550de919b2e731e54d89de55efe4a0442deda3
Ashwaq3 <eng.ashwaq.h.a@hotmail.com>
2023-01-12 10:45:28 +0300

    merge

--- Nano/Views/PreTravelChecklist.swift
+++ Nano/Views/PreTravelChecklist.swift
@@ -8,24 +8,31 @@
 import SwiftUI
 
 struct PreTravelChecklist: View {
-  
     init(){
         UITableView.appearance().backgroundColor = UIColor(Color.clear)
     }
     
+
+
     @State private var data = ["Electronics", "Documents","Clothes","Essentials"]
   
     
+
     @State private var selections = Set<String>()
     var body: some View {
         VStack{
-            Text("pre_travel_checklist").frame(maxWidth: .infinity,
-                                               alignment: .leading).font(Font.custom(Locale.preferredLanguages[0] == "en" ? "Gilroy-Medium": "Tajawal-Medium", size: 21)).foregroundColor(.white).padding(EdgeInsets(top: 32, leading: 24, bottom: 16, trailing: 24))
+            Text("pre_travel_checklist")
+                .accessibilityLabel(Text("pre_travel_checklist"))
+                .frame(maxWidth: .infinity,
+                                               alignment:
+                    .leading)
+            .font(Font.custom(Locale.preferredLanguages[0] == "en" ? "Gilroy-Medium": "Tajawal-Medium", size: 21)).foregroundColor(.white).padding(EdgeInsets(top: 32, leading: 24, bottom: 16, trailing: 24))
             
             NavigationView {
                 ScrollView {
                     ForEach(data, id: \.self) { dataItem in
                         PackingSections(section: dataItem)
+                        
                             .animation(.default)
                     }
                  
